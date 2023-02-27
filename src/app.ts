615a4dec62aa1b4628c1c9446c937f0668ab5c9b
Thiago <thiagocezar32@gmail.com>
2023-02-27 14:40:40 -0300

    :books: documentation for api

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import "express-async-errors";
 import express from "express";
 import "reflect-metadata";
+import swaggerUI from "swagger-ui-express"
+import swaggerDocument from "../swagger.json"
 
 import { appRoutes } from "./routes";
 import handleErrorMidleware from "./middlewares/handleError.midleware";
@@ -8,6 +10,7 @@ import handleErrorMidleware from "./middlewares/handleError.midleware";
 
 const app = express()
 app.use(express.json())
+app.use("/documentation", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 appRoutes(app);
 app.use(handleErrorMidleware)
