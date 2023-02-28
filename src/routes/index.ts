202d5df3a01055ce04a3ece78b23a98d231fec7b
Murillo Miguel <mmiguel.skn@gmail.com>
2023-02-28 16:02:42 -0300

    :sparkles: applyed service to authorization

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Express } from "express";
 import { announcementRoutes } from "./announcements.routes";
 import { HelloRoutes } from "./helloWord.routes";
+import sessionRoutes from "./session.routes";
 
 export const appRoutes = (app: Express) => {
     app.use("/", HelloRoutes());
     app.use("/announcement", announcementRoutes);
+    app.use("/login", sessionRoutes);
 };
