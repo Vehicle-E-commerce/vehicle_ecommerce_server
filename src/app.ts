3b236f5b3b40ddfa554b95dd371a6ba47385b617
Ingleson <jovemtksstranger@gmail.com>
2023-02-28 17:47:07 -0400

    :sparkles: adding cors to api implementation

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "express-async-errors";
 import express from "express";
+import cors from "cors";
 import "reflect-metadata";
 import swaggerUI from "swagger-ui-express"
 import swaggerDocument from "../swagger.json"
@@ -10,6 +11,15 @@ import handleErrorMidleware from "./middlewares/handleError.midleware";
 
 const app = express()
 app.use(express.json())
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", 'GET, POST, PATCH, DELETE')
+
+  next();
+})
+
 app.use("/documentation", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 appRoutes(app);
